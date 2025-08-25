1c180596abb2fc25cd17f96c63ee36d6f999f58f
zenokan-4ik <zenokan4ik@gmail.com>
2025-08-25 12:25:26 +0300

    added picture handling

--- main.py
+++ main.py
@@ -74,37 +74,74 @@ async def process_callback(callback_query: CallbackQuery, state: FSMContext):
             await send_msg(callback_query.message, msgs.REQUEST)
             await state.set_state(RequestState.waiting_for_request)
 
+from io import BytesIO
+import aiohttp
+
 @dp.message(RequestState.waiting_for_request)
 async def process_request(msg: Message, state: FSMContext):
-    user_request = msg.text
+    user_request = msg.text or msg.caption or ""
     
     # Process the request
     data = {
         'id': msg.from_user.id,
-        'username': '@'+msg.from_user.username,
+        'username': '@' + msg.from_user.username if msg.from_user.username else str(msg.from_user.id),
         'request': user_request
     }
-    resp = api.post('addrequest/', {
+
+    files = {}
+    photo_data = []
+
+    if msg.photo:
+        photos = msg.photo
+        largest_photo = photos[-1]
+        file_info = await bot.get_file(largest_photo.file_id)
+        downloaded_file = await bot.download_file(file_info.file_path)
+        
+        photo_data.append({
+            'file_id': largest_photo.file_id,
+            'bytes': downloaded_file.read()
+        })
+        
+        downloaded_file.seek(0)
+        files['photo'] = ('photo.jpg', downloaded_file)
+
+    resp = api.post('addrequest/', data={
         'tg_id': data['id'],
         'username': data['username'],
-        'request': data['request']
-    })
+        'request': data['request'],
+    }, files=files)
+
     print('## ', resp)
     print(data['username'], ' | ', data['id'])
-    
+
     successed_sends = 0
+    admin_message_text = f'<b>Новый запрос!!!!</b>\n\n\nПользователь: {data["username"]}\nID пользователя: {data["id"]}\nТекст запроса: {data["request"]}'
+
     for admin_id in admins.ADMINS_ID:
-        if await send_msg_by_id(
-            admin_id,
-            f'<b>Новый запрос!!!!</b>\n\n\nПользователь: {data['username']}\nId пользователя: {data['id']}\nТекст запроса: {data['request']}'
-        ):
+        try:
+            if photo_data:
+                await bot.send_photo(
+                    admin_id,
+                    photo=photo_data[0]['bytes'],
+                    caption=admin_message_text,
+                    parse_mode='HTML'
+                )
+            else:
+                await bot.send_message(
+                    admin_id,
+                    admin_message_text,
+                    parse_mode='HTML'
+                )
             successed_sends += 1
+        except Exception as e:
+            print(f"Failed to send to admin {admin_id}: {e}")
+
+    await msg.answer(f"Спасибо! Ваш запрос отправлен {successed_sends} {'участнику' if successed_sends == 1 else 'участникам'}\nВаш запрос:\n{user_request}")
     
-    await msg.answer(f"Спасибо! Ваш запрос отправлен {successed_sends} {'участнику' if successed_sends%10 == 1 else 'участникам'}\nВаш запрос:\n{user_request}")
-    await msg.answer(f"Если количество участников равно 0, то произошла ошибка. Можете обратиться к любому из участников напрямую с помоью кнопки 'Контакты'")
+    if successed_sends == 0:
+        await msg.answer("Если количество участников равно 0, то произошла ошибка. Можете обратиться к любому из участников напрямую с помощью кнопки 'Контакты'")
     
     await state.clear()
-    
     await msg.answer("Чем еще могу помочь?", reply_markup=main_menu())
 
 async def main() -> None:
--- tools/api.py
+++ tools/api.py
@@ -4,10 +4,13 @@ class Api:
     def __init__(self, base_url: str):
         self.url = base_url
         
-    def post(self, endpoint: str, data: dict):
-        response = requests.post(self.url+endpoint, data)
+    def post(self, endpoint: str, data: dict, files: dict = None):
+        if files:
+            response = requests.post(self.url + endpoint, data=data, files=files)
+        else:
+            response = requests.post(self.url + endpoint, data=data)
         return response.content.decode()
     
     def get(self, endpoint: str, data: dict):
-        response = requests.get(self.url+endpoint, data)
+        response = requests.get(self.url + endpoint, data=data)
         return response.content.decode()
